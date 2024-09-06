a3544c55ca82309a64d8950532475011b1a9aa1e
yundolee <48395932+yundolee@users.noreply.github.com>
2024-09-06 11:02:54 +0900

    Add files via upload

---
+++ 9.6.py
@@ -0,0 +1,63 @@
+#print(1, 2, 3)
+#print('Hello', 'Python')
+
+'''
+print(1,2,3, sep=',')
+print(4,5,6, sep=', ')
+print('Hello','Python',sep='')
+print(1920,1080, sep='x')
+'''
+'''
+print(1, 2, 3)
+print(1, 2, 3, sep='\n')
+
+print('1\n2\n3')
+'''
+
+'''
+print(1, end='')
+print(2, end='')
+print(3)
+print(1, end=' ')
+print(2, end=' ')
+print(3)
+'''
+'''
+year = 2000
+month = 10
+day = 27
+hour = 11
+minute = 43
+second = 59
+
+print(year, month, day, sep='/',end = ' ')
+print(hour, minute, second, sep=':')
+'''
+
+#3 > 1
+#1 == 1.0
+
+"""
+korean = 92
+english = 47
+mathematics =86
+science = 81
+
+print(korean >= 50 and english >= 50 and mathematics >= 50 and science >= 50)
+
+
+
+
+a,b,c,d = map(int, input().split())
+print(a >= 90 and b > 80 and c > 85 and d >= 80)
+ ㄴ map,int,input,split을 기억못함 다음에는 기억해보자
+"""
+
+
+
+
+
+
+
+
+
